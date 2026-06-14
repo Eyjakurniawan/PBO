ffaeef2ed0c8a432a9f0af4e38342e55ce742e61
ramlannapitupulu <ramlanleonardo10@gmail.com>
2026-06-14 22:57:14 +0700

    Update Tugas.cpp

--- Tugas.cpp
+++ Tugas.cpp
@@ -91,4 +91,93 @@ void hitungfinansial() override {
     }
 };
 
-int main ();
+int main () {
+    vector<pegawai*> daftarPegawai;  //Untuk menampung database pegawai sementara
+    int pilihanMenu;
+    string namaPegawai, noPegawai;
+
+    do {
+        cout <<"^^^Menu Utama^^^" <<endl;
+        cout <<"1. Input Data Pegawai" <<endl;
+        cout <<"2. Tampilan Data" <<endl;
+        cout <<"3. Keluar" <<endl;
+        cout <<"Pilihan: ";
+        cin >> pilihanMenu;
+        cin.ignore();
+
+        if (pilihanMenu == 1) {
+            cout <<"\n---Menu Input Data Pegawai---" <<endl;
+            cout <<"Masukkan Nama Pegawai: ";
+            getline(cin, namaPegawai);
+            cout <<"Masukkan Nomor Pegawai: ";
+            getline(cin, noPegawai);
+
+            int pilihanJabatan;
+            pegawai* pegawaiBaru = nullptr;
+
+            while (true) {  //Loop untuk validasi jabatan
+                cout <<"\nDaftar Jabatan Pegawai" <<endl;
+                cout <<"1. Komisaris" <<endl;
+                cout <<"2. Direktur" <<endl;
+                cout <<"3. Manajer" <<endl;
+                cout <<"4. Staff" <<endl;
+                cin >> pilihanJabatan;
+
+                if (pilihanJabatan == 1) {
+                    pegawaiBaru = new Komisaris(namaPegawai, noPegawai);
+                    break;
+                } else if (pilihanJabatan == 2) {
+                    pegawaiBaru = new Direktur(namaPegawai, noPegawai);
+                    break;
+                } else if (pilihanJabatan == 3) {
+                    pegawaiBaru = new Manajer(namaPegawai, noPegawai);
+                    break;
+                } else if (pilihanJabatan == 4) {
+                    pegawaiBaru = new Staf(namaPegawai, noPegawai);
+                    break;
+                } else {
+                    cout <<"Pesan: No Salah, Ulangi !" <<endl;
+                }
+            }
+            if (pegawaiBaru != nullptr) { //Gabung & Simpan Data
+                pegawaiBaru ->hitungfinansial();
+                daftarPegawai.push_back(pegawaiBaru);
+                cout <<"\nPesan: Data Pegawai Tersimpan" <<endl;
+            }
+        } else if (pilihanMenu == 2) {
+            cout <<"\nMenu Daftar Pegawai" <<endl;
+            if (daftarPegawai.empty()) {
+                cout <<"Belum ada data pegawai yang tersimpan" <<endl;
+            } else {
+                for (size_t i = 0; i < daftarPegawai.size(); i++) {
+                    cout <<"Pegawai ke-" << (i + 1) << endl;
+                    daftarPegawai[i] -> tampilkandata();
+                }
+            }
+            int subPilihan;
+            cout <<"\nMau lanjut kemana?" <<endl;
+            cout <<"1. Kembali ke awal (Menu)" <<endl;
+            cout <<"2. Selesai (Keluar Aplikasi)" <<endl;
+            cout <<"Pilihan Anda: ";
+            cin >> subPilihan;
+
+            if (subPilihan == 2) {
+                cout <<"Pesan: Terimakasih Bla bla bla" <<endl;
+                break;
+            }
+        } else if (pilihanMenu == 3) {
+            cout <<"\nPesan: Terimakasih Bla bla bla" <<endl;
+            break;
+        } else {
+            cout <<"\nPilihan anda salah. Masukkan ANGKA 1-3!\n\n";
+        }
+    } while (pilihanMenu != 3);
+
+    for (auto peg : daftarPegawai) {
+        delete peg;
+    }
+    daftarPegawai.clear();
+
+    cout <<"\nProgram Selesai" <<endl;
+    return 0;
+};
